90d39a3725d0112034e2a276cf54b15adae05764
Practicante3 <practicante3@cnet.co.cr>
2023-11-23 00:19:18 -0600

    cambios

--- src/app/info-repo/info-repo.component.ts
+++ src/app/info-repo/info-repo.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Repo } from '../core/model/repo.model';
+import { DataService } from '../core/service/data.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-info-repo',
   templateUrl: './info-repo.component.html',
@@ -10,21 +13,25 @@ import { Repo } from '../core/model/repo.model';
 
 
 export class InfoRepoComponent implements OnInit{
-  @Input() repo: Repo | undefined;
+ // @Input() repo: Repo | undefined;
 
   images: any[] | undefined;
   public imagenPrincipal: string;
+  repo: Repo | undefined;
 
   
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, public dataService: DataService, private router: Router) {
+    this.repo = dataService.repoData;
     this.images = this.repo?.image;
     this.imagenPrincipal = "";
 
    }
 
   ngOnInit() {
-    
-    console.log(this.repo?.linkPage); // Mostrará 'valor1'
+    if (!this.dataService.repoData) {
+      this.router.navigate(['/']);
+    }
+
     this.mostrarGaleria();
   }
 
--- src/app/workplace/workplace.component.ts
+++ src/app/workplace/workplace.component.ts
@@ -3,6 +3,7 @@ import { GithubService } from '../github.service';
 import { Router } from '@angular/router';
 import { InfoRepoComponent } from '../info-repo/info-repo.component';
 import { Repo } from '../core/model/repo.model';
+import { DataService } from '../core/service/data.service';
 
 @Component({
   selector: 'app-workplace',
@@ -18,42 +19,43 @@ export class WorkplaceComponent implements OnInit {
   imagePoke: any[] = ["/pokemon/poke0.jpg","/pokemon/poke1.jpg","/pokemon/poke2.jpg","/pokemon/poke3.jpg"];
   skillsPoke: any[] = ["IONIC","c#"];
 
+  imageRDA: any[] = ["/rda/RDA0.jpg","/rda/RDA1.jpg","/rda/RDA2.jpg","/rda/RDA3.jpg"];
+  skillsRDA: any[] = ["IONIC","c#"];
+
+  imageCMC: any[] = ["/cocoMilkC/CM0.jpg","/cocoMilkC/CM1.jpg","/cocoMilkC/CM2.jpg","/cocoMilkC/CM3.jpg"];
+  skillsCMC: any[] = ["IONIC","c#"];
+
+  imageCMA: any[] = ["/cocoMilkA/cmA0.jpg","/cocoMilkA/cmA1.jpg","/cocoMilkA/cmA2.jpg","/cocoMilkA/cmA3.jpg"];
+  skillsCMA: any[] = ["IONIC","c#"];
+
   repos: Repo[] = [
    /* {  name: 'IONIC', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis ullam, ut quisquam illo et non tempora reiciendis accusamus nesciunt maiores labore alias animi laborum provident doloremque facilis, quibusdam voluptatem.', image: "img1.jpg",linkPage:"",linkGit:"https://github.com/"},
     {  name: 'IONIC POKEMON', description:'', image: "img2.jpg",linkPage:"",linkGit:""},*/
     {  name: 'BIOSIMET', description:'Pagina Web de Geolocalizacion de CNET', image: this.imageCnet,linkPage:"",linkGit:"", skills: this.skillsCnet, about: this.aboutCnet},
     {  name: 'IONIC POKEMON', description:'Pagina Web realizada en IONIC', image: this.imagePoke ,linkPage:"https://kendall2106.github.io/IonicPokemon/",linkGit:"https://github.com/Kendall2106/IonicPokemon.git", skills:this.skillsPoke, about: this.aboutCnet},
-    {  name: 'BIOSIMET', description:'Pagina Web de Geolocalizacion de CNET', image: this.imageCnet,linkPage:"",linkGit:"", skills: this.skillsCnet, about: this.aboutCnet},
-    {  name: 'IONIC POKEMON', description:'Pagina Web realizada en IONIC', image: this.imagePoke ,linkPage:"https://kendall2106.github.io/IonicPokemon/",linkGit:"https://github.com/Kendall2106/IonicPokemon.git", skills:this.skillsPoke, about: this.aboutCnet},
-    {  name: 'BIOSIMET', description:'Pagina Web de Geolocalizacion de CNET', image: this.imageCnet,linkPage:"",linkGit:"", skills: this.skillsCnet, about: this.aboutCnet},
-    {  name: 'IONIC POKEMON', description:'Pagina Web realizada en IONIC', image: this.imagePoke ,linkPage:"https://kendall2106.github.io/IonicPokemon/",linkGit:"https://github.com/Kendall2106/IonicPokemon.git", skills:this.skillsPoke, about: this.aboutCnet},
-    {  name: 'BIOSIMET', description:'Pagina Web de Geolocalizacion de CNET', image: this.imageCnet,linkPage:"",linkGit:"", skills: this.skillsCnet, about: this.aboutCnet},
-    {  name: 'IONIC POKEMON', description:'Pagina Web realizada en IONIC', image: this.imagePoke ,linkPage:"https://kendall2106.github.io/IonicPokemon/",linkGit:"https://github.com/Kendall2106/IonicPokemon.git", skills:this.skillsPoke, about: this.aboutCnet},
+    {  name: 'Reportes de Averias', description:'Pagina Web de reportes de averias hecho para el ICE', image: this.imageRDA,linkPage:"",linkGit:"https://github.com/Kendall2106/ReporteDeAverias.git", skills: this.skillsRDA, about: this.aboutCnet},
+    {  name: 'HOTEL COCOMILK CLIENTE', description:'Pagina Web cliente', image: this.imageCMC ,linkPage:"",linkGit:"", skills:this.skillsCMC, about: this.aboutCnet},
+    {  name: 'HOTEL COCOMILK ADMINISTRADOR', description:'Pagina Web Administrador', image: this.imageCMA,linkPage:"",linkGit:"", skills: this.skillsCMA, about: this.aboutCnet},
   ];
 
-  constructor(private githubService: GithubService, private router: Router){
+  constructor(private githubService: GithubService, private router: Router, private dataService: DataService){
 
   }
 
   ngOnInit(): void{
-    /*this.githubService.loadRepos().subscribe( res => {
-      this.repos = res;
-    })*/
+
   }
   
-  /*navegarAComponenteDestino() {
-    //this.router.navigate(['/componente1']);
-    this.router.navigate(['/componente1'], { state: { data: this.repos } });
 
-  }*/
-
-  valor: string = 'Hola desde el componente padre';
+  navegarAComponenteDestino(repo: Repo) {
+    this.dataService.repoData = repo;
+    this.router.navigate(['/componente1']);
+  }
 
   // Luego, puedes establecer el valor de la propiedad "dato" en el componente hijo.
-  navegarAComponenteDestino(repo: Repo) {
-    //NotificacionDialogComponent.prototype.notificar(this.respuesta);
+  /*navegarAComponenteDestino(repo: Repo) {
     this.router.navigate(['/componente1']);
     InfoRepoComponent.prototype.repo = repo;
-  }
+  }*/
 
 }
